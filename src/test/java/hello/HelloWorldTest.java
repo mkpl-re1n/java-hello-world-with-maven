aa0595bae4843afd9560957ae32c850191c7458f
Fiffah <itsmefiffah@gmail.com>
2023-06-13 22:00:29 +0700

    Create HelloWorldTest.java

---
+++ src/test/java/hello/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package hello;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorldTest {
+    @Test
+    public void testSayHello() {
+        Greeter greeter = new Greeter();
+        assertEquals("Hello, World!", greeter.sayHello());
+    }
+}
